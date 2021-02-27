0f5ace473c7d17eeb56d5c2477d71ef034952bf8
Cameron Mitchell <Cameronghm@gmail.com>
2021-02-27 16:05:24 +0000

    Updated to be tested on front-end

--- app.js
+++ app.js
@@ -5,89 +5,79 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const uuid = require('uuid');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
-app.use(express.json);
-app.use(session({
-    
-}))
-
+app.use(express.json());
 const Unis = ['durham', 'warwick'];
 
 app.post('/api/signup', function(req, resp) {
-    if (!(req.body.name) || !(validator.isAlphanumeric(req.body.name))) {
+    //Validating Name
+    if (!(req.body.name) || !(/^[a-zA-Z]+$/.test(req.body.name))) {
         resp.json({"error-field":"name", "error": "Need a valid name"});
     }
-    if (!validator.isStrongPassword(req.body.password)) {
-        resp.json({"error-field":"password", "error": "Need a valid password (minimum length 8, must have at least one uppercase, lowercase and symbol)"});
+    //Validating Password
+    if (!(req.body.name) || !(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/.test(req.body.password))) {
+        resp.json({"error-field":"password", "error": "Need a valid password (minimum length 8, must have at least one uppercase, lowercase, number and symbol)"});
     }
+    //Validating Uni
     if (!Unis.includes((req.body.uni).toLowerCase())) {
         resp.json({"error-field":"uni", "error": "Need a valid Uni"});
     }
-    if (!(validator.isEmail(req.body.uniemail)) || !(validator.contains(req.body.uniemail, '.ac.uk'))) { //Add check for uni email address to selected uni
+    //Validating Uni Email
+    if (!(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.ac.uk$/.test(req.body.uniemail)) || !(req.body.uniemail.includes(req.body.uni))) { //Add check for uni email address to selected uni
         resp.json({"error-field":"uniemail", "error": "Need a valid uni Email"});
     }
-    if (validator.isEmpty(req.body.useremail)) {
+    /*db.get(`SELECT uni_email FROM users WHERE uni_email = ${req.body.uniemail}`, function (row) {
+        if (row != undefined) {
+            resp.json({"error-field":"uniemail", "error": "Uni Email already in use"});
+        }
+    });*/
+    //Validating User Email
+    if (!req.body.useremail) {
         req.body.useremail = user.body.uniemail;
     }
-    if ((!validator.isEmpty(req.body.useremail)) && (!validator.isEmail(req.body.useremail))) {
+    if (!(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(req.body.useremail))) {
         resp.json({"error-field":"useremail", "error": "Need a User Email"});
     }
-    
+    //Splitting Name
     const first_name = req.body.name.split(' ')[0];
-    if (req.body.name.split(' ')[1] == undefined) {
-        const last_name = '';
+    let last_name = '';
+    if (req.body.name.split(' ')[1]) {
+        last_name = req.body.name.replace(req.body.name.split(' ')[0], '');
     }
-    else {
-        const last_name = req.body.name.replace(req.body.name.split(' ')[0], '');
-    }
-    const password = req.body.password;
-    db.run(`INSERT INTO users (first_name, last_name, uni_email, contact_email, password)
-        VALUES (${first_name}, ${last_name}, ${req.body.uniemail}, ${req.body.useremail}, )
-    );`);
+    //Adding data to database
+    /*bcrypt.hash(req.body.password, 10, function(err, hash) {
+        db.run(`INSERT INTO users (first_name, last_name, uni_email, contact_email, password)
+        VALUES (${first_name}, ${last_name}, ${req.body.uniemail}, ${req.body.useremail}, ${hash}));`)
+    });*/
+    //Sending OK response
+    resp.sendStatus(200);
 });
 
-app.post('/api/signin'), function(req, resp) {
+/*app.post('/api/signin', function(req, resp) {
     if (!(validator.isEmail(req.body.uniemail)) || !(validator.contains(req.body.uniemail, '.ac.uk'))) { //Add check for uni email address to selected uni
         resp.json({"error-field":"uniemail", "error": "Need a valid uni Email"});
     }
-    // Database comparing stuff for username and password
-}
-
-//Database Stuff
-const path = require('path');
-const e = require('express');
-const dbPath = path.resolve(__dirname, 'data/data.db');
-const sqlite3 = require('sqlite3').verbose(); // verbose gives longer traces in case of error
-
-let db = new sqlite3.Database(dbPath, (err) => {
-    if (err) {
-        return console.error(err.message);
+    if (!req.body.password) {
+        resp.json({"error-field":"password", "error": "Need to enter a password"});
     }
-});
+    db.get(`SELECT uni_email, password FROM users WHERE uni_email = ${req.body.uniemail}`, function (row) {
+        if (row == undefined) {
+            resp.json({"error-field":"uniemail", "error": "Uni Email not found"});
+        }
+        else {
+            bcrypt.hash(req.body.password, 10, function(err, hash) {
+                bcrypt.compare(row.password, hash, function(err, result) {
+                    if (result) {
 
-db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS users (
-        user_id INTEGER PRIMARY KEY,
-        first_name TEXT NOT NULL,
-        last_name TEXT NOT NULL,
-        uni_email TEXT NOT NULL,
-        contact_email TEXT NOT NULL,
-        password_hash TEXT NOT NULL,
-        password_salt TEXT NOT NULL,
-        year_group INTEGER
-    );`);
-    db.run(`CREATE TABLE IF NOT EXISTS module_lookup (
-        user_id INTEGER NOT NULL,
-        module_id INTEGER NOT NULL,
-        status INTEGER NOT NULL
-    );`);
-});
-
-
-db.close((err) => {
- if (err) {
-     return console.error(err.message);
- }
-});
+                    }
+                    else {
+                        resp.json({"error-field":"password", "error": "Incorrect Password"});
+                    }
+                })
+            });
+        }
+    });
+});*/
 
 module.exports = app;
